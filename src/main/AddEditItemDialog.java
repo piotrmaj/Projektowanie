584bd6e40027141cf527266af2334f900959fc0c
mario1992 < mario1992>
2014-10-23 01:26:36 +0200

    s

---
+++ src/main/AddEditItemDialog.java
@@ -0,0 +1,195 @@
+package main;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JComboBox;
+import javax.swing.BoxLayout;
+import javax.swing.Box;
+import java.awt.GridLayout;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.JTextField;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class AddEditItemDialog extends JDialog {
+
+	private final JPanel contentPanel = new JPanel();
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			AddEditItemDialog dialog = new AddEditItemDialog();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			AddEditItemDialog dialog = new AddEditItemDialog();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			AddEditItemDialog dialog = new AddEditItemDialog();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			AddEditItemDialog dialog = new AddEditItemDialog();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public AddEditItemDialog() {
+		setBounds(100, 100, 324, 208);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		GridBagLayout gbl_contentPanel = new GridBagLayout();
+		gbl_contentPanel.columnWidths = new int[]{74, 0, 0};
+		gbl_contentPanel.rowHeights = new int[]{30, 0, 0, 0, 0};
+		gbl_contentPanel.columnWeights = new double[]{1.0, 1.0, Double.MIN_VALUE};
+		gbl_contentPanel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		contentPanel.setLayout(gbl_contentPanel);
+		{
+			JLabel lblNewLabel = new JLabel("Produkt");
+			lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+			GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+			gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
+			gbc_lblNewLabel.gridx = 0;
+			gbc_lblNewLabel.gridy = 0;
+			contentPanel.add(lblNewLabel, gbc_lblNewLabel);
+		}
+		{
+			JComboBox comboBox = new JComboBox();
+			GridBagConstraints gbc_comboBox = new GridBagConstraints();
+			gbc_comboBox.insets = new Insets(0, 0, 5, 0);
+			gbc_comboBox.fill = GridBagConstraints.HORIZONTAL;
+			gbc_comboBox.gridx = 1;
+			gbc_comboBox.gridy = 0;
+			contentPanel.add(comboBox, gbc_comboBox);
+		}
+		{
+			JLabel lblNewLabel_1 = new JLabel("Nazwa");
+			GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
+			gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 5);
+			gbc_lblNewLabel_1.gridx = 0;
+			gbc_lblNewLabel_1.gridy = 1;
+			contentPanel.add(lblNewLabel_1, gbc_lblNewLabel_1);
+		}
+		{
+			textField = new JTextField();
+			GridBagConstraints gbc_textField = new GridBagConstraints();
+			gbc_textField.insets = new Insets(0, 0, 5, 0);
+			gbc_textField.fill = GridBagConstraints.HORIZONTAL;
+			gbc_textField.gridx = 1;
+			gbc_textField.gridy = 1;
+			contentPanel.add(textField, gbc_textField);
+			textField.setColumns(10);
+		}
+		{
+			JLabel lblNewLabel_2 = new JLabel("Cena");
+			GridBagConstraints gbc_lblNewLabel_2 = new GridBagConstraints();
+			gbc_lblNewLabel_2.insets = new Insets(0, 0, 5, 5);
+			gbc_lblNewLabel_2.gridx = 0;
+			gbc_lblNewLabel_2.gridy = 2;
+			contentPanel.add(lblNewLabel_2, gbc_lblNewLabel_2);
+		}
+		{
+			textField_1 = new JTextField();
+			GridBagConstraints gbc_textField_1 = new GridBagConstraints();
+			gbc_textField_1.insets = new Insets(0, 0, 5, 0);
+			gbc_textField_1.fill = GridBagConstraints.HORIZONTAL;
+			gbc_textField_1.gridx = 1;
+			gbc_textField_1.gridy = 2;
+			contentPanel.add(textField_1, gbc_textField_1);
+			textField_1.setColumns(10);
+		}
+		{
+			JLabel lblNewLabel_3 = new JLabel("Ilo\u015B\u0107 ca\u0142kowita");
+			GridBagConstraints gbc_lblNewLabel_3 = new GridBagConstraints();
+			gbc_lblNewLabel_3.insets = new Insets(0, 0, 0, 5);
+			gbc_lblNewLabel_3.gridx = 0;
+			gbc_lblNewLabel_3.gridy = 3;
+			contentPanel.add(lblNewLabel_3, gbc_lblNewLabel_3);
+		}
+		{
+			textField_2 = new JTextField();
+			GridBagConstraints gbc_textField_2 = new GridBagConstraints();
+			gbc_textField_2.fill = GridBagConstraints.HORIZONTAL;
+			gbc_textField_2.gridx = 1;
+			gbc_textField_2.gridy = 3;
+			contentPanel.add(textField_2, gbc_textField_2);
+			textField_2.setColumns(10);
+		}
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				JButton okButton = new JButton("zapisz");
+				okButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent arg0) {
+						setVisible(false);
+					}
+				});
+				okButton.setActionCommand("OK");
+				buttonPane.add(okButton);
+				getRootPane().setDefaultButton(okButton);
+			}
+			{
+				JButton cancelButton = new JButton("Cancel");
+				cancelButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent arg0) {
+						setVisible(false);
+					}
+				});
+				cancelButton.setActionCommand("Cancel");
+				buttonPane.add(cancelButton);
+			}
+		}
+	}
+
+}
---
+++ src/main/ConfirmSendingDialog.java
@@ -0,0 +1,211 @@
+package main;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.GridBagLayout;
+import javax.swing.JLabel;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.BoxLayout;
+import javax.swing.Box;
+import javax.swing.JRadioButton;
+import javax.swing.JTextField;
+import javax.swing.JComboBox;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class ConfirmSendingDialog extends JDialog {
+
+	private final JPanel contentPanel = new JPanel();
+	private JTextField textField;
+
+	/**
+	 * Create the dialog.
+	 */
+	public ConfirmSendingDialog() {
+		setBounds(100, 100, 450, 300);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(new BorderLayout(0, 0));
+		{
+			JPanel panel = new JPanel();
+			contentPanel.add(panel, BorderLayout.NORTH);
+			GridBagLayout gbl_panel = new GridBagLayout();
+			gbl_panel.columnWidths = new int[]{0, 0, 0};
+			gbl_panel.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0};
+			gbl_panel.columnWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+			gbl_panel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+			panel.setLayout(gbl_panel);
+			{
+				JLabel lblId = new JLabel("id");
+				GridBagConstraints gbc_lblId = new GridBagConstraints();
+				gbc_lblId.insets = new Insets(0, 0, 5, 5);
+				gbc_lblId.gridx = 0;
+				gbc_lblId.gridy = 0;
+				panel.add(lblId, gbc_lblId);
+			}
+			{
+				JLabel lblNewLabel = new JLabel("New label");
+				GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+				gbc_lblNewLabel.insets = new Insets(0, 0, 5, 0);
+				gbc_lblNewLabel.gridx = 1;
+				gbc_lblNewLabel.gridy = 0;
+				panel.add(lblNewLabel, gbc_lblNewLabel);
+			}
+			{
+				JLabel lblNewLabel_1 = new JLabel("Produkt");
+				GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
+				gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 5);
+				gbc_lblNewLabel_1.gridx = 0;
+				gbc_lblNewLabel_1.gridy = 1;
+				panel.add(lblNewLabel_1, gbc_lblNewLabel_1);
+			}
+			{
+				JLabel lblNewLabel_2 = new JLabel("New label");
+				GridBagConstraints gbc_lblNewLabel_2 = new GridBagConstraints();
+				gbc_lblNewLabel_2.insets = new Insets(0, 0, 5, 0);
+				gbc_lblNewLabel_2.gridx = 1;
+				gbc_lblNewLabel_2.gridy = 1;
+				panel.add(lblNewLabel_2, gbc_lblNewLabel_2);
+			}
+			{
+				JLabel lblNewLabel_3 = new JLabel("Nazwa");
+				GridBagConstraints gbc_lblNewLabel_3 = new GridBagConstraints();
+				gbc_lblNewLabel_3.insets = new Insets(0, 0, 5, 5);
+				gbc_lblNewLabel_3.gridx = 0;
+				gbc_lblNewLabel_3.gridy = 2;
+				panel.add(lblNewLabel_3, gbc_lblNewLabel_3);
+			}
+			{
+				JLabel lblNewLabel_4 = new JLabel("New label");
+				GridBagConstraints gbc_lblNewLabel_4 = new GridBagConstraints();
+				gbc_lblNewLabel_4.insets = new Insets(0, 0, 5, 0);
+				gbc_lblNewLabel_4.gridx = 1;
+				gbc_lblNewLabel_4.gridy = 2;
+				panel.add(lblNewLabel_4, gbc_lblNewLabel_4);
+			}
+			{
+				JLabel lblNewLabel_5 = new JLabel("Dost\u0119pna ilo\u015B\u0107");
+				GridBagConstraints gbc_lblNewLabel_5 = new GridBagConstraints();
+				gbc_lblNewLabel_5.insets = new Insets(0, 0, 5, 5);
+				gbc_lblNewLabel_5.gridx = 0;
+				gbc_lblNewLabel_5.gridy = 3;
+				panel.add(lblNewLabel_5, gbc_lblNewLabel_5);
+			}
+			{
+				JLabel lblNewLabel_6 = new JLabel("New label");
+				GridBagConstraints gbc_lblNewLabel_6 = new GridBagConstraints();
+				gbc_lblNewLabel_6.insets = new Insets(0, 0, 5, 0);
+				gbc_lblNewLabel_6.gridx = 1;
+				gbc_lblNewLabel_6.gridy = 3;
+				panel.add(lblNewLabel_6, gbc_lblNewLabel_6);
+			}
+			{
+				JLabel lblNewLabel_7 = new JLabel("Cena");
+				GridBagConstraints gbc_lblNewLabel_7 = new GridBagConstraints();
+				gbc_lblNewLabel_7.insets = new Insets(0, 0, 5, 5);
+				gbc_lblNewLabel_7.gridx = 0;
+				gbc_lblNewLabel_7.gridy = 4;
+				panel.add(lblNewLabel_7, gbc_lblNewLabel_7);
+			}
+			{
+				JLabel lblNewLabel_8 = new JLabel("New label");
+				GridBagConstraints gbc_lblNewLabel_8 = new GridBagConstraints();
+				gbc_lblNewLabel_8.insets = new Insets(0, 0, 5, 0);
+				gbc_lblNewLabel_8.gridx = 1;
+				gbc_lblNewLabel_8.gridy = 4;
+				panel.add(lblNewLabel_8, gbc_lblNewLabel_8);
+			}
+			{
+				JLabel lblNewLabel_9 = new JLabel("Jednostka");
+				GridBagConstraints gbc_lblNewLabel_9 = new GridBagConstraints();
+				gbc_lblNewLabel_9.insets = new Insets(0, 0, 0, 5);
+				gbc_lblNewLabel_9.gridx = 0;
+				gbc_lblNewLabel_9.gridy = 5;
+				panel.add(lblNewLabel_9, gbc_lblNewLabel_9);
+			}
+			{
+				JLabel lblNewLabel_10 = new JLabel("New label");
+				GridBagConstraints gbc_lblNewLabel_10 = new GridBagConstraints();
+				gbc_lblNewLabel_10.gridx = 1;
+				gbc_lblNewLabel_10.gridy = 5;
+				panel.add(lblNewLabel_10, gbc_lblNewLabel_10);
+			}
+		}
+		{
+			JPanel panel = new JPanel();
+			contentPanel.add(panel, BorderLayout.CENTER);
+			panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
+			{
+				Box verticalBox = Box.createVerticalBox();
+				panel.add(verticalBox);
+				{
+					JRadioButton rdbtnNewRadioButton = new JRadioButton("Nowe zlecenie");
+					verticalBox.add(rdbtnNewRadioButton);
+				}
+				{
+					JLabel lblNewLabel_11 = new JLabel("Potwierdz wys\u0142anie nast\u0119puj\u0105cej ilo\u015Bci");
+					verticalBox.add(lblNewLabel_11);
+				}
+				{
+					Box horizontalBox = Box.createHorizontalBox();
+					verticalBox.add(horizontalBox);
+					{
+						JLabel lblIlo = new JLabel("Ilo\u015B\u0107");
+						horizontalBox.add(lblIlo);
+					}
+					{
+						textField = new JTextField();
+						horizontalBox.add(textField);
+						textField.setColumns(1);
+					}
+				}
+				{
+					JRadioButton rdbtnNewRadioButton_1 = new JRadioButton("Rezerwacje");
+					verticalBox.add(rdbtnNewRadioButton_1);
+				}
+				{
+					JLabel lblWybierzZZarezerwowanych = new JLabel("Wybierz z zarezerwowanych");
+					verticalBox.add(lblWybierzZZarezerwowanych);
+				}
+				{
+					JComboBox comboBox = new JComboBox();
+					verticalBox.add(comboBox);
+				}
+			}
+		}
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				JButton okButton = new JButton("OK");
+				okButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent arg0) {
+						setVisible(false);
+					}
+				});
+				okButton.setActionCommand("OK");
+				buttonPane.add(okButton);
+				getRootPane().setDefaultButton(okButton);
+			}
+			{
+				JButton cancelButton = new JButton("Cancel");
+				cancelButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent arg0) {
+						setVisible(false);
+					}
+				});
+				cancelButton.setActionCommand("Cancel");
+				buttonPane.add(cancelButton);
+			}
+		}
+	}
+
+}
