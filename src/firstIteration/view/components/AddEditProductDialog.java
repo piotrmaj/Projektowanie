be4d32e2c1670b1531fbc01c49d00578f06a4f36
Olga <gerasiko@ee.pw.edu.pl>
2014-11-16 14:45:46 +0100

    Prezenter - przypadki użycia: +Dodaj/Edytuj StoreItem +Potwierdz wyslanie/wydanie +Usun StoreItem +Usun Product +Dodaj/Edytuj Product

---
+++ src/firstIteration/view/components/AddEditProductDialog.java
@@ -0,0 +1,20 @@
+package firstIteration.view.components;
+
+import javax.swing.JDialog;
+
+import dto.ProductDTO;
+
+public class AddEditProductDialog  extends JDialog{
+
+	ProductDTO product;
+
+	public AddEditProductDialog() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void setProduct(ProductDTO product2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/firstIteration/view/components/DeleteProductDialog.java
@@ -0,0 +1,11 @@
+package firstIteration.view.components;
+
+import dto.ProductDTO;
+
+public class DeleteProductDialog {
+
+	public DeleteProductDialog(ProductDTO product) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
